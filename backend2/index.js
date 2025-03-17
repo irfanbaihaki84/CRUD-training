6fcb117052d0980c28a128542cc9df78b36986e8
irfanbaihaki84 <irfanbaihaki84@gmail.com>
2025-03-18 02:27:05 +0700

    baihaki

--- backend2/index.js
+++ backend2/index.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import UsersRoutes from '../routes/UsersRoute';
-import MiddlewareLogRequest from '../middleware/logs';
-const port = process.env.PORT || 7000;
-const app = express();
+// import UsersRoutes from '../routes/UsersRoute';
+// import MiddlewareLogRequest from '../middleware/logs';
 
 dotenv.config();
 
+const app = express();
+const port = process.env.PORT || 7000;
+
 // MIDDLEWARE
-app.use(MiddlewareLogRequest);
+// app.use(MiddlewareLogRequest);
 
 app.use(express.json());
 
-app.use('/users', UsersRoutes);
+// app.use('/users', UsersRoutes);
 
 app.use('/', (req, res) => {
   res.send('SELAMAT DATANG.');
