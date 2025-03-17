541215a7abb2a80815827a7c5c9d049e603020db
irfanbaihaki84 <irfanbaihaki84@gmail.com>
2025-03-18 01:23:15 +0700

    baihaki

--- backend1/src/index.js
+++ backend1/src/index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 require('dotenv').config();
 // const datas = require('./data.js');
+const mysql = require('mysql2');
 const UsersRoutes = require('../routes/UsersRoute');
 const port = 5000;
 const app = express();
 
+// KONEKSI KE DATABASE
+const dbPool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'db_1',
+});
+
 app.use(express.json());
 
 app.use('/users', UsersRoutes);
+
+app.use('/test', (req, res) => {
+  dbPool.query('SELECT * FROM users', (err, rows) => {
+    if (err) {
+      res.json({ message: 'Koneksi Gagal!' });
+    }
+
+    return res.json({
+      message: 'Koneksi Sukses',
+      data: rows,
+    });
+  });
+});
 
 app.use('/', (req, res) => {
   res.send('SELAMAT DATANG.');
--- readme.md
+++ readme.md
@@ -11,3 +11,10 @@ git push -u origin main
 git remote add origin https://github.com/irfanbaihaki84/CRUD-training.git
 git branch -M main
 git push -u origin main
+
+## TAHAPAN TUGAS-TUGAS
+
+** Buat Repository
+** Buat file .gitignore
+** Buat file README.MD
+** Clone Repo Dari Github
