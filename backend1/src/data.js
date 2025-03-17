0b2a100251cf2ad8305631fa74e3e4c4651e8592
irfanbaihaki84 <irfanbaihaki84@gmail.com>
2025-03-18 00:45:27 +0700

    datas, index

---
+++ backend1/src/data.js
@@ -0,0 +1,24 @@
+const datas = {
+  users: [
+    {
+      id: 1,
+      nama: 'andi',
+      email: 'andi@email.com',
+      address: 'tangerang',
+    },
+    {
+      id: 2,
+      nama: 'budi',
+      email: 'budi@email.com',
+      address: 'karawaci',
+    },
+    {
+      id: 3,
+      nama: 'celin',
+      email: 'celin@email.com',
+      address: 'bitung',
+    },
+  ],
+};
+
+module.exports = datas;
--- backend1/src/index.js
+++ backend1/src/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 require('dotenv').config();
+const datas = require('./data.js');
 const port = 5000;
+const app = express();
 
-app.express();
 app.use(express.json());
 
-app.listen('port', () => console.log(`Server running on port: ${port}`));
+app.use('/users', (req, res) => {
+  //   console.log('data: ', datas);
+
+  try {
+    res.json({
+      message: 'GET dummy data success.',
+      data: datas.users,
+    });
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// app.use('/', (req, res) => {
+//   res.send('SELAMAT DATANG.');
+// });
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
